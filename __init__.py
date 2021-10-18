dd2444039fd0a1467119274756cad561c2b38066
Imre NAGY <crash@veluna.eu>
2021-10-18 15:58:25 +0200

    separate identify method

--- __init__.py
+++ __init__.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-
 from calibre.ebooks.metadata.book.base import Metadata
 from calibre import browser
 from calibre.ebooks.metadata.sources.base import Source
@@ -22,49 +21,34 @@ class VelunaMetadata(Source):
 
     capabilities = frozenset(['identify'])
 
-    SEARCH_URL = 'https://moly.hu/kereses?utf8=%E2%9C%93&q='
-    BASE_URL = 'https://moly.hu/'
-
     def identify(self, log, result_queue, abort, title=None, authors=None, identifiers={}, timeout=30):
-        log.info('veluna begin')
+        base_url = 'https://moly.hu/'
 
+        search_param = f'{authors[0]}  {title}'
+        books = get_for_search(base_url, f'kereses?utf8=%E2%9C%93&q={search_param}')
 
-        query = self.SEARCH_URL + f'{authors[0]}  {title}'
-        log.info(query)
+        for book in books:
+            result_queue.put(book)
 
-
-        br = browser()
-        response = br.open(query)
-        raw = response.read().strip()
-        raw = raw.decode('utf-8', errors='replace')
-        content = clean_ascii_chars(raw)
-
-        search_result = list(parse_search.parse_search_results(content))
-        log.info(search_result)
-
-
-        bookurl = self.BASE_URL + search_result[0][2]
-        resp2 = br.open_novisit(bookurl)
-        raw2 = resp2.read().strip()
-        # raw2 = raw.decode('utf-8', errors='replace')
-        content2 = clean_ascii_chars(raw2)
-        parser = parse_book.HtmlParser()
-        book_res = parser.extract_book_details(content2)
-        log.info(book_res)
-
-        meta = Metadata(book_res['title'], book_res['authors'])
-        result_queue.put(meta)
-
-        # books = [
-        #     Metadata('aaaaaaaaaaaaaaa', ['author']),
-        #     # Metadata('sdfsdf sdfsdafsda fsd', ['another autor']),
-        #     # Metadata('sdfdsfasdf dsfsd fsdf sdaf', ['dfsdfsd fads fgrfggfh d fg']),
-        #     # Metadata('title2', ['author2'])
-        # ]
-
-        # for book in books:
-        #     book.source_relevance = 0
-        #     result_queue.put(book)
-
-        log.info('veluna end')
         return None
+
+
+def get_url_content(url):
+    br = browser()
+    response = br.open(url)
+    raw = response.read().strip()
+    raw = raw.decode('utf-8', errors='replace')
+    return clean_ascii_chars(raw)
+
+
+def get_for_search(base_url, search_param):
+    search_url = base_url + search_param
+    search_page_source = get_url_content(search_url)
+    search_result = parse_search.parse_search_results(search_page_source)
+
+    for result in search_result:
+        book_url = base_url + result[2]
+        book_page_source = get_url_content(book_url)
+        book_data = parse_book.HtmlParser().extract_book_details(book_page_source)
+        yield Metadata(book_data['title'], book_data['authors'])
+
